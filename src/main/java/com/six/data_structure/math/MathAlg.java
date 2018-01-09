dbb27f5b568e90f24ae9aced6ededbd382bc90eb
liusong <you@example.com>
2018-01-09 09:47:16 +0800

    update

--- src/main/java/com/six/data_structure/math/MathAlg.java
+++ src/main/java/com/six/data_structure/math/MathAlg.java
@@ -9,7 +9,46 @@ import java.util.Objects;
 public class MathAlg {
 
 	/**
+	 * n=7= 0000 0111 1= 0000 0001 n=7= (1+2)+4 n=5= 1+4
+	 * 矩阵求幂
+	 * @param num
+	 * @param n
+	 * @return
+	 */
+	public static double powByDivision(double num, int n) {
+		if (num == Double.NaN) {
+			return 0d;
+		}
+		if (n == 0) {
+			return 1d;
+		}
+		if (num >= Double.POSITIVE_INFINITY) {
+			return Double.POSITIVE_INFINITY;
+		}
+		if (num <= Double.NEGATIVE_INFINITY) {
+			if (n % 2 == 0) {
+				return Double.POSITIVE_INFINITY;
+			} else {
+				return Double.NEGATIVE_INFINITY;
+			}
+		}
+		double temp = num;
+		double result = 1;
+		for (; n > 0; n >>= 1) {
+			if ((n & 1) == 1) {
+				result *= temp;
+			}
+			temp *= temp;
+		}
+		if (n < 0) {
+			result = 1d / result;
+		}
+		return result;
+	}
+
+	/**
 	 * int[][] result=a*b,a.row==b.col
+	 * 
 	 * @param a
 	 * @param b
 	 * @return
@@ -23,21 +62,29 @@ public class MathAlg {
 		int row = a[0].length;
 		int col = b.length;
 		int[][] result = new int[b.length][a[0].length];
-		int sumCount=a.length;
+		int sumCount = a.length;
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
 				int temp = 0;
 				for (int m = 0; m < sumCount; m++) {
-					int a1=a[m][i];
-					int b1=b[j][m];
-					temp +=a1 * b1;
+					temp += a[m][i] * b[j][m];
 				}
 				result[j][i] = temp;
 			}
 		}
 		return result;
 	}
-	
+
+	public static int[][] matrixMultiplicationByDivide(int[][] a, int[][] b) {
+		Objects.requireNonNull(a);
+		Objects.requireNonNull(b);
+		if (a.length <= 0 || b.length <= 0 || a.length != b[0].length) {
+			throw new IllegalArgumentException();
+		}
+		return null;
+
+	}
+
 	public static int[][] matrixMultiplicationByStrassen(int[][] a, int[][] b) {
 		Objects.requireNonNull(a);
 		Objects.requireNonNull(b);
@@ -45,6 +92,6 @@ public class MathAlg {
 			throw new IllegalArgumentException();
 		}
 		return null;
-		
+
 	}
 }
--- src/test/java/com/six/data_structure/MathTest.java
+++ src/test/java/com/six/data_structure/MathTest.java
@@ -1,6 +1,6 @@
 package com.six.data_structure;
 
-import com.six.data_structure.MyMath;
+import com.six.data_structure.math.MathAlg;
 
 /**   
  * @author sixliu   
@@ -11,17 +11,10 @@ import com.six.data_structure.MyMath;
 public class MathTest {
 
 	public static void main(String[] args) {
-		System.out.println(0x1.0p53);
-		long dtal=Double.doubleToRawLongBits(10);
-		int dta=(int)(dtal >> 32);
-		System.out.println(dta);
-		double result=1.0 / 0.0;
-		System.out.println(result);
-		System.out.println(Math.pow(2,2.5));
-		System.out.println(Math.pow(2,2));
-		System.out.println(Math.pow(2,0.5));
-		System.out.println(Math.pow(2,2)*Math.pow(2,0.5));
-		System.out.println(MyMath.pow(1999999999999999999999.0,10));
+		double a=111.11d;
+		int b=5;
+		System.out.println(Math.pow(a,b));
+		System.out.println(MathAlg.powByDivision(a,b));
 	}
 
 }
